c7b243dbe78063beff925e430901e62f561e5426
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-09-16 02:53:32 +0100

    "Added fs package, updated categoryService.js to use fs and path modules, and modified image upload and resize functionality"

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,5 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+
 const asyncHandler = require("express-async-handler");
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { v4: uuidv4 } = require("uuid");
@@ -31,15 +34,25 @@ exports.uploadBrandImage = uploadSingleImage("image");
 
 exports.resizeImage = asyncHandler(async (req, res, next) => {
   const filename = `category-${uuidv4()}-${Date.now()}.jpeg`;
-  console.log("filename", req.file);
-  if (req.file) {
-    await sharp(req.file.buffer)
-      .resize(600, 600)
-      .toFormat("jpeg")
-      .jpeg({ quality: 95 })
-      .toFile(`./uploads/categories/${filename}`);
-    req.body.image = filename;
-    console.log("req.body.image", req.body.image);
+  const uploadDir = path.join(__dirname, "../uploads/categories");
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+  }
+
+  try {
+    if (req.file) {
+      await sharp(req.file.buffer)
+        .resize(600, 600)
+        .toFormat("jpeg")
+        .jpeg({ quality: 95 })
+        .toFile(path.join(uploadDir, filename));
+      req.body.image = filename;
+      console.log("req.body.image", req.body.image);
+    }
+  } catch (error) {
+    console.error("Error processing image:", error);
+    return res.status(500).json({ message: "Error processing image" });
   }
 
   next();
